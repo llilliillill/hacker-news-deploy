b525d601632a9763dc584bacd6b96c5d4416ee97
Istratov Alexey <istratov_alexey@bk.ru>
2024-12-05 18:25:12 +0400

    Updates

---
+++ assets/date-62ac0f56.js
@@ -0,0 +1,1 @@
+const r="https://hacker-news.firebaseio.com/v0",t=e=>new Date(e*1e3).toLocaleString("ru",{year:"numeric",month:"numeric",day:"numeric"}),a=e=>Number(e.replace(/([0-9]+).([0-9]+).([0-9]+)/,"$3$2$1"));export{r as A,a,t as g};
